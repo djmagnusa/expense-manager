d8ead83e7e7dab8c1133bcdbc6af5f163ab159ad
Pratush <pratush.bh@gmail.com>
2021-05-10 03:15:55 +0530

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
     },
     module: {
@@ -50,7 +50,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',//'eval-cheap-module-source-map', //if isProduction is true then source-map else eval-cheap
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/' //publicPath lets us specify where those bundled assets should live
     }
      
   };
