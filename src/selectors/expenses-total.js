b42beb71e9eb628bbf1a1563f2d9a48cee0d1513
Pratush <pratush.bh@gmail.com>
2021-05-11 12:03:34 +0530

    More test suites and updated snapshots

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,6 +1,6 @@
 export default (expenses) => {
     return expenses
-        .map((expense) => expense.amount)
-        .reduce((sum, value) => sum + value, 0);
+        .map((expense) => expense.amount)  //explicitly returning the array of the amounts of all the expenses
+        .reduce((sum, value) => sum + value, 0); //initializing accumlator value with 0
   };
   
